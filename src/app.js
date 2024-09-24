bea03d84a25c6e282734b04a521903965029ea78
Roderick Shoolbraid <rdxenv@gmail.com>
2024-09-23 18:15:53 -0700

    express, and middle ware

---
+++ src/app.js
@@ -0,0 +1,25 @@
+// src/app.js
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api', routes);
+
+// Serve static files (your SPA)
+app.use(express.static('public'));
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+module.exports = app;
