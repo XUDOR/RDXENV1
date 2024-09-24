caa55bce11ce72a53ba7def7b65598cb4727fde4
Roderick Shoolbraid <rdxenv@gmail.com>
2024-09-23 18:16:11 -0700

    test route

---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+// src/routes/index.js
+const express = require('express');
+const router = express.Router();
+const testController = require('../controllers/testController');
+
+router.get('/test', testController.test);
+
+module.exports = router;
